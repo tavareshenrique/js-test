292211b42c76c9c82672367cebda8fb13cc56f32
Henrique Tavares <ihenrits@gmail.com>
2022-01-03 19:25:27 -0300

    Jest watch mode: observar alterações nos arquivos

--- project-1/src/lib/calculator.js
+++ project-1/src/lib/calculator.js
@@ -1,1 +1,10 @@
-module.exports.sum = (num1, num2) => num1 + num2;
+module.exports.sum = (num1, num2) => {
+  const int1 = parseInt(num1);
+  const int2 = parseInt(num2);
+
+  if (isNaN(int1) || isNaN(int2)) {
+    throw new Error("Cannot sum non-numeric values");
+  }
+
+  return int1 + int2;
+}
--- project-1/src/lib/calculator.spec.js
+++ project-1/src/lib/calculator.spec.js
@@ -3,3 +3,17 @@ const { sum } = require('./calculator')
 it('should sum 2 and 2 and the result must be 4', () => {
   expect(sum(2, 2)).toBe(4);
 })
+
+it('should sum 3 and 2 even if one of them is a string and result must be 5', () => {
+  expect(sum("3", 2)).toBe(5);
+})
+
+it('should throw an error if what is provided to the method cannot be summed', () => {
+  expect(() => sum("", "2")).toThrowError();
+
+  expect(() => sum([2, 2])).toThrowError();
+
+  expect(() => sum({})).toThrowError();
+
+  expect(() => sum()).toThrowError();
+})
