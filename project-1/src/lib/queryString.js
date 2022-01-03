85f5b2b0d4f484e33da9a4ee5c5063c11eefa5cc
Henrique Tavares <ihenrits@gmail.com>
2022-01-03 19:52:16 -0300

    Mais casos de teste para query string

--- project-1/src/lib/queryString.js
+++ project-1/src/lib/queryString.js
@@ -1,4 +1,10 @@
 module.exports.queryString = obj =>
-  Object.keys(obj)
-    .map(key => `${key}=${obj[key]}`)
+  Object.entries(obj)
+    .map(([key, value]) => {
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        throw new Error('Objects are not allowed as values');
+      }
+
+      return `${key}=${value}`;
+    })
     .join('&');
--- project-1/src/lib/queryString.spec.js
+++ project-1/src/lib/queryString.spec.js
@@ -9,6 +9,29 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Henrique&age=25');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Henrique',
+      abilities: ['React', 'TypeScript'],
+    };
+
+    expect(queryString(obj)).toBe('name=Henrique&abilities=React,TypeScript');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Henrique',
+      abilities: {
+        first: 'React',
+        second: 'TypeScript',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
 
 // describe(('Query string to object') => {
