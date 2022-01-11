ee1f46de05be08feba311b5e9d2599b7e1445f58
Henrique Tavares <ihenrits@gmail.com>
2022-01-11 20:35:51 -0300

    Módulo 3 - Testar componente Search - parte 1.1

--- module-3/react-nextjs-test/components/search.spec.js
+++ module-3/react-nextjs-test/components/search.spec.js
@@ -1,10 +1,22 @@
 import Search from './search';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+const doSearch = jest.fn();
 
 describe('<Search />', () => {
-  it('should render Search component', () => {
-    render(<Search />);
+  it('should render a form', () => {
+    render(<Search doSearch={doSearch} />);
 
-    expect(screen.getByTestId('search')).toBeInTheDocument()
+    expect(screen.getByRole('form')).toBeInTheDocument()
+  })
+
+  it('should call props.doSearch() when form is submitted', () => {
+    render(<Search doSearch={doSearch} />);
+
+    const form = screen.getByRole('form');
+
+    fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledTimes(1)
   })
 })
