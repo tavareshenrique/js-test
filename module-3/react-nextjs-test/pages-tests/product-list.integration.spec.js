c15fe9ec2e9e2e2edcf9f2123133c98b9455fc60
Henrique Tavares <ihenrits@gmail.com>
2022-01-12 20:24:55 -0300

    Integration Tests - parte 3

--- module-3/react-nextjs-test/pages-tests/product-list.integration.spec.js
+++ module-3/react-nextjs-test/pages-tests/product-list.integration.spec.js
@@ -1,11 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import { makeServer } from '../miragejs/server';
 
 import ProductList from '../pages';
 
+const renderProductList = () => render(<ProductList />);
+
 describe('ProductList', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({
+      environment: 'test',
+    });
+  })
+
+  afterEach(() => {
+    server.shutdown();
+  })
+
   it('should render ProductList', () => {
-    render(<ProductList />);
+    renderProductList();
 
     expect(screen.getByTestId('product-list')).toBeInTheDocument()
+  })
+
+  it('should render the ProductCard component 10 times', async () => {
+    server.createList('product', 10)
+    
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(10)
+    })
   })
 })
