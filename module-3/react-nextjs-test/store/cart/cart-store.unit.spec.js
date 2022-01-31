9132caf3967650ac1ccf4d939dee15a16c938e41
Henrique Tavares <ihenrits@gmail.com>
2022-01-31 20:19:49 -0300

    store - actions

--- module-3/react-nextjs-test/store/cart/cart-store.unit.spec.js
+++ module-3/react-nextjs-test/store/cart/cart-store.unit.spec.js
@@ -1,12 +1,47 @@
 import { renderHook, act } from '@testing-library/react-hooks';
+import { makeServer } from '../../miragejs/server'
+
 import { useCartStore } from './';
 
 describe('Cart Store', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  })
+
   it('should return open equals false on initial state', () => {
     const { result } = renderHook(() => useCartStore());
 
     expect(result.current.state.open).toBe(false);
-  })
+  });
+
+  it('should return an empty array for products on initial state', () => {
+    const { result } = renderHook(() => useCartStore());
+
+    expect(Array.isArray(result.current.state.products)).toBe(true);
+    expect(result.current.state.products).toHaveLength(0);
+  });
+
+  it('should', async () => {
+    const products = server.createList('product', 2);
+
+    const { result } = renderHook(() => useCartStore());
+
+    const { actions: { add } } = result.current;
+
+    for (const product of products) {
+      act(() => {
+        add(product);
+      })
+    }
+
+    expect(result.current.state.products).toHaveLength(2);
+  });
 
   it('should toggle open state', () => {
     const { result } = renderHook(() => useCartStore());
@@ -14,9 +49,9 @@ describe('Cart Store', () => {
     const { actions: { toggle } } = result.current;
 
     act(() =>  toggle());
-    expect(result.current.state.open).toBe(true);
+    expect(result.current.state.open).toBe(false);
 
     act(() =>  toggle());
-    expect(result.current.state.open).toBe(false);
-  })
+    expect(result.current.state.open).toBe(true);
+  });
 })
