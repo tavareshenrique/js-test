52b20ba0b93dc78525acb806228a91aafd1423dc
Henrique Tavares <ihenrits@gmail.com>
2022-02-09 19:12:35 -0300

    store: remove and removeAll

--- module-3/react-nextjs-test/store/cart/cart-store.unit.spec.js
+++ module-3/react-nextjs-test/store/cart/cart-store.unit.spec.js
@@ -67,4 +67,44 @@ describe('Cart Store', () => {
 
     expect(result.current.state.products).toHaveLength(0);
   });
+
+  it('should remove a product from the store', () => {
+    const [product1, product2] = server.createList('product', 2);
+
+    const { actions: { add, remove } } = result.current;
+
+    act(() => {
+      add(product1);
+      add(product2);
+    })
+
+    expect(result.current.state.products).toHaveLength(2);
+
+    act(() => {
+      remove(product1);
+    })
+
+    expect(result.current.state.products).toHaveLength(1);
+    expect(result.current.state.products[0]).toEqual(product2);
+  });
+
+  it('should clear cart', () => {
+    const products = server.createList('product', 2);
+
+    const { actions: { add, removeAll } } = result.current;
+
+    act(() => {
+      for (const product of products) {
+        add(product);
+      }
+    });
+
+    expect(result.current.state.products).toHaveLength(2);
+
+    act(() => {
+      removeAll();
+    })
+
+    expect(result.current.state.products).toHaveLength(0);
+  })
 })
