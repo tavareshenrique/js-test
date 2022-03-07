65ff2421119267dbabc85f894df9eefc81fa11c2
Henrique Tavares <ihenrits@gmail.com>
2022-03-07 20:15:08 -0300

    Integration Tests

--- module-4/src/controllers/home.controller.unit.spec.js
+++ module-4/src/controllers/home.controller.unit.spec.js
@@ -1,5 +1,17 @@
-describe('Controller > Home', () => {
-  it('should do something', () => {
-    expect(1 + 1).toEqual(2);
+import { index } from './home.controller';
+
+describe('Controllers > Home', () => {
+  const res = {
+    status: jest.fn(() => res),
+    json: jest.fn(() => res),
+  };
+
+  it('should do something', async () => {
+    await index(null, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ welcome: 'Welcome Stranger!' });
   });
 });
